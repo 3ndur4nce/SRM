4ffc414936384af9803d85e95eb359aace5a68b4
3ndur4nce <simon@endur4.com>
2022-11-05 17:42:57 -0400

    Added addscript

--- WebPage/place/postactions.php
+++ WebPage/place/postactions.php
@@ -17,9 +17,26 @@ if($place == "assets" && $sched == "save"){
     if(isset($script)){
         $shell = trim($_POST["shell"]);
         $data = trim($_POST["script"]);
-        $editscript = "UPDATE scripts set shell = ?, data = ? where id = $script";
+        $scriptname = trim($_POST["scriptname"]);
+        $editscript = "UPDATE scripts set name = ?, shell = ?, data = ? where id = $script";
         if($stmt = mysqli_prepare($link, $editscript)){
-            mysqli_stmt_bind_param($stmt, "ss", $shell, $data);
+            mysqli_stmt_bind_param($stmt, "sss", $scriptname, $shell, $data);
+            if(mysqli_stmt_execute($stmt)){
+                header("location: ?a=scripts&script=$script");
+                exit;
+            }else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+    }
+}elseif($place == "scripts" && $do == "newscript"){
+    if(isset($script)){
+        $shell = trim($_POST["shell"]);
+        $data = trim($_POST["script"]);
+        $scriptname = trim($_POST["scriptname"]);
+        $addscript = "INSERT into scripts (name, shell, data) values (?, ?, ?)";
+        if($stmt = mysqli_prepare($link, $addscript)){
+            mysqli_stmt_bind_param($stmt, "sss", $scriptname, $shell, $data);
             if(mysqli_stmt_execute($stmt)){
                 header("location: ?a=scripts&script=$script");
                 exit;
--- WebPage/place/scripts.php
+++ WebPage/place/scripts.php
@@ -2,6 +2,7 @@
     <a>Scripts</a><br>
     <?php
     
+    echo "<a href='?a=scripts&script=newscript' class='collumnbutton'>--> New <--</a>";
     $getscriptsqry = mysqli_query($link, "select * from scripts ORDER BY name ASC");
     while($row = mysqli_fetch_array($getscriptsqry)){
         echo "<a href='?a=scripts&script=".$row['id']."' class='collumnbutton'>".$row['name']."</a>";
@@ -9,28 +10,35 @@
 echo "</div>";
 
     if(isset($script)){
-        $getscriptsqry = mysqli_query($link, "select * from scripts where id = $script");
-        while($row = mysqli_fetch_array($getscriptsqry)){
-            $scriptname = $row['name'];
-            $scriptdata = $row['data'];
-            $shell = $row['shell'];
+        if($script != 'newscript'){
+            $do = "scriptedit";
+            $getscriptsqry = mysqli_query($link, "select * from scripts where id = $script");
+            while($row = mysqli_fetch_array($getscriptsqry)){
+                $scriptname = $row['name'];
+                $scriptdata = $row['data'];
+                $shell = $row['shell'];
+            }
+            if($shell == 'powershell'){
+                $powershell = "powershell' selected='selected";
+                $cmd = "cmd";
+                $bash = "bash";
+            }elseif($shell == 'cmd'){
+                $powershell = "powershell";
+                $cmd = "cmd' selected='selected";
+                $bash = "bash";
+            }elseif($shell == 'bash'){
+                $powershell = "powershell";
+                $cmd = "cmd";
+                $bash = "bash' selected='selected";
+            }
+        }else{
+            $do = "newscript";
         }
-        if($shell == 'powershell'){
-            $powershell = "powershell' selected='selected";
-            $cmd = "cmd";
-            $bash = "bash";
-        }elseif($shell == 'cmd'){
-            $powershell = "powershell";
-            $cmd = "cmd' selected='selected";
-            $bash = "bash";
-        }elseif($shell == 'bash'){
-            $powershell = "powershell";
-            $cmd = "cmd";
-            $bash = "bash' selected='selected";
-        }
+        
         echo "<div class='editcollumn'>
-        <a>$scriptname</a><br>
-        <form action='?a=scripts&script=$script&do=scriptedit' method='POST'>
+        <a>Name</a><br>
+        <form action='?a=scripts&script=$script&do=$do' method='POST'>
+            <input type='text' name='scriptname' class='input' placeholder='Script Name' value='$scriptname'><br>
             <select id='shell' name='shell'>
                 <option value='$cmd'>CMD</option>
                 <option value='$powershell'>PowerShell</option>
@@ -41,3 +49,6 @@ echo "</div>";
             </form>
         ";
     }
+
+
+    
